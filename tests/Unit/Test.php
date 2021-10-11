6d38aae3060dbe69e3ceea7271096abad10b906b
Seb Kay <seb@sebkay.com>
2021-10-11 18:32:11 +0100

    Remove references to dot env

--- tests/Unit/Test.php
+++ tests/Unit/Test.php
@@ -10,9 +10,6 @@ abstract class Test extends TestCase
 
     public function setUp(): void
     {
-        //---- Load environment variables
-        \Dotenv\Dotenv::createImmutable(__DIR__, '../../.tests.env')->load();
-
         //---- Create new Faker object
         $this->faker = \Faker\Factory::create();
     }
