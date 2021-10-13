dc1d8a20592e9e2e5f7f034cfc09db6b155171f2
Seb Kay <seb@sebkay.com>
2021-10-13 20:25:21 +0100

    Add Omnisend service integration

---
+++ src/Services/Omnisend.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Arbalest\Services;
+
+use Arbalest\Values\Configs\OmnisendConfig;
+use Arbalest\Values\EmailAddress;
+use DateTime;
+
+class Omnisend extends Service
+{
+    public function __construct(
+        array $config
+    ) {
+        parent::__construct(new OmnisendConfig($config));
+
+        $this->http = new \GuzzleHttp\Client([
+            'base_uri' => "https://api.omnisend.com/v3/",
+            'headers'  => [
+                'X-API-KEY' => $this->config->get('api_key'),
+            ],
+        ]);
+    }
+
+    /**
+     * Create an array for adding/updating an email contact
+     *
+     * @param EmailAddress $email_address
+     * @param bool $subscribed
+     * @return arrat
+     */
+    protected function arrayForEmailContact(EmailAddress $email_address, bool $subscribed = true)
+    {
+        $status = $subscribed == true ? 'subscribed' : 'unsubscribed';
+
+        return [
+            'identifiers' => [
+                [
+                    'type'     => 'email',
+                    'id'       => $email_address->get(),
+                    'channels' => [
+                        'email' => [
+                            'status'     => $status,
+                            'statusDate' => (new DateTime('now'))->format("Y-m-d\TH:i:s.000\Z"),
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function subscribe(
+        EmailAddress $email_address
+    ): bool {
+        try {
+            $response = $this->post("contacts", [
+                'json' => $this->arrayForEmailContact($email_address, true),
+            ]);
+
+            return $response->getStatusCode() == 200 ? true : false;
+        } catch (\Exception $e) {
+            throw new \Exception('There was an error subscribing that email address.', (int) $e->getCode());
+        }
+    }
+
+    public function unsubscribe(
+        EmailAddress $email_address
+    ): bool {
+        try {
+            $response = $this->post("contacts", [
+                'json' => $this->arrayForEmailContact($email_address, false),
+            ]);
+
+            return $response->getStatusCode() == 200 ? true : false;
+        } catch (\Exception $e) {
+            throw new \Exception('There was an error unsubscribing that email address.', (int) $e->getCode());
+        }
+    }
+}
