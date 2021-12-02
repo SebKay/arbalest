2833a8b9e594ee4707ee0e4066cad210057d2c04
Seb Kay <seb@sebkay.com>
2021-12-02 23:09:53 +0000

    Convert unit tests for Arbalest\Services\Mailchimp to Pest

--- tests/Unit/Services/MailchimpTest.php
+++ tests/Unit/Services/MailchimpTest.php
@@ -3,15 +3,7 @@
 namespace ArbalestTests\Unit\Services;
 
 use Arbalest\Services\Mailchimp;
-use ArbalestTests\Test;
 
-class MailchimpTest extends Test
-{
-    /**
-     * @test
-     */
-    public function it_can_hash_an_email_address()
-    {
-        $this->assertSame('b642b4217b34b1e8d3bd915fc65c4452', Mailchimp::subscriberHash('test@test.com'));
-    }
-}
+test('It can hash an email address', function () {
+    expect(Mailchimp::subscriberHash('test@test.com'))->toBe('b642b4217b34b1e8d3bd915fc65c4452');
+});
